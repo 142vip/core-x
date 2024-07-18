855d54bfa8d003623e9850f4aee2f4943f0d895e
chufan <mmdapl@163.com>
2024-07-19 00:58:31 +0800

    chore: update

--- packages/fairy-cli/src/utils/exec-command.ts
+++ packages/fairy-cli/src/utils/exec-command.ts
@@ -1,7 +1,17 @@
-import type { Options } from 'execa'
+import * as execa from 'execa'
 
-export async function execCommand(cmd: string, args: string[], options?: Options) {
-  const { execa } = await import('execa')
-  const res = await execa(cmd, args, options)
-  return (res?.stdout as string)?.trim() || ''
+// export async function execCommand(cmd: string, args: string[], options?: Options) {
+//   const { execa } = await import('execa')
+//   const res = await execa(cmd, args, options)
+//   return (res?.stdout as string)?.trim() || ''
+// }
+
+export async function execCommand(cmd: string) {
+  const execResult = await execa.commandSync(cmd)
+
+  // 非零退出
+  if (execResult.exitCode !== 0) {
+    process.exit(execResult.exitCode)
+  }
+  return (execResult?.stdout)?.trim() || ''
 }
