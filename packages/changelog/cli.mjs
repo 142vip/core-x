fa7376d611afdd1cd4da9d4b67a0f7c6385a2df8
chufan <mmdapl@163.com>
2024-08-07 15:27:13 +0800

    refactor(@142vip/changelog): 结构调整，移除cac等模块，替换为commander模块

--- packages/changelog/cli.mjs
+++ packages/changelog/cli.mjs
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 // eslint-disable-next-line antfu/no-import-dist
-import './dist/changelog.mjs'
+// import './dist/index.mjs'
+import {changelogMain} from "./dist/index.mjs";
+
+changelogMain()
