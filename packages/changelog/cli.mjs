0ee1fa35bb84919d689c21b7ab418cdd9f6f10ca
chufan <mmdapl@163.com>
2024-08-07 15:30:28 +0800

    chore: update

--- packages/changelog/cli.mjs
+++ packages/changelog/cli.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-// eslint-disable-next-line antfu/no-import-dist
+
 // import './dist/index.mjs'
-import {changelogMain} from "./dist/index.mjs";
+import { changelogMain } from './dist/index.mjs'
 
 changelogMain()
