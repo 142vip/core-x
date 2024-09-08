6fc94c60e07b635a83e3ef556de769e3b555f4cd
chufan <mmdapl@163.com>
2024-09-08 14:43:19 +0800

    feat(@142vip/utils): 修复`execShell`函数执行异常，支持同步执行命令

--- packages/utils/src/shell.ts
+++ packages/utils/src/shell.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk'
 import { name, version } from '../package.json'
-import { commandStandardExecutor } from './exec'
+import { execCommand } from './exec'
 import { VipLogger } from './logger'
 
 export interface ShellCommand {
@@ -46,8 +46,11 @@ export async function execShell(commands: ShellCommand[] | string | ShellCommand
     //   endLabel: `${description} (执行开始)`,
     // })
 
-    // 标准命令执行器
-    commandStandardExecutor(command)
+    // 同步执行，标准命令执行器
+    const execResult = await execCommand(command)
+
+    // 打印输出结果
+    vipLog.info(`\n${execResult.stdout}\n`)
 
     // 脚本结束
     // vipLog.log('', {
