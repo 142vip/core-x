a7e667568f4b36d9badb5b167a7e97d1d668b314
chufan <mmdapl@163.com>
2025-04-28 18:54:51 +0800

    chore: update

--- test.js
+++ test.js
@@ -1,51 +1,61 @@
 import * as td from 'typedoc'
 
 async function testMain() {
-// Application.bootstrap also exists, which will not load plugins
-// Also accepts an array of option readers if you want to disable
-// TypeDoc's tsconfig.json/package.json/typedoc.json option readers
   const app = await td.Application.bootstrapWithPlugins({
   // Note: This accepts globs, do not pass paths with backslash path separators!
-    entryPoints: ['packages/*'],
     entryPointStrategy: 'packages',
-    name: '靓仔',
-    includeVersion: false,
-    out: 'apis',
-    theme: 'default',
+    entryPoints: [
+      'packages/axios',
+      'packages/changelog',
+      'packages/release-version',
+      'packages/fairy-cli',
+      'packages/utils',
+    ],
+    name: '@142vip/core-x',
+    titleLink: 'https://github.com/142vip/core-x',
+    includeVersion: true,
+    out: 'dist/apis',
+    theme: 'typedoc-github-theme',
     exclude: [
       '**/*.test.ts',
       '**/node_modules/**',
     ],
-    tsconfig: 'tsconfig.json',
+    // tsconfig: 'tsconfig.package.json',
     plugin: [
-      // 'typedoc-plugin-markdown',
-      // 'typedoc-vitepress-theme',
+      'typedoc-github-theme',
     ],
-    // readme: 'README.md',
-    // sidebar: {
-    //   autoConfiguration: true,
-    //   format: 'vitepress',
-    //   pretty: false,
-    //   collapsed: true,
-    // },
     cleanOutputDir: true,
-    excludePrivate: true, // 导出不包含private声明内容
-    excludeProtected: true, // 导出不包含protected声明内容
+    excludeNotDocumented: false,
+    excludeExternals: false,
+    hideGenerator: true,
+    categorizeByGroup: false,
+    projectDocuments: ['CHANGELOG.md', 'README.md'],
+    packageOptions: {
+      includeVersion: false,
+      projectDocuments: ['CHANGELOG.md', 'README.md'],
+      entryPoints: ['src/index.ts'],
+      readme: 'none', // 不生成 README 页面
+    },
+    lang: 'zh',
+    navigation: {
+      includeFolders: false,
+      includeCategories: true,
+      includeGroups: false,
+      compactFolders: true,
+      excludeReferences: false,
+    },
+    sluggerConfiguration: {
+      lowercase: true,
+    },
+    githubPages: true,
+    includeHierarchySummary: true,
+    logLevel: 'Verbose',
   })
 
-  // May be undefined if errors are encountered.
   const project = await app.convert()
 
   if (project) {
-  // Generate configured outputs
-  //   await app.generateOutputs(project)
-
-    // Alternatively...
-    const outputDir = 'dist/static/apis'
-    // Generate HTML rendered docs
-    await app.generateDocs(project, outputDir)
-    // Alternatively generate JSON output
-    await app.generateJson(project, `${outputDir}/docs.json`)
+    await app.generateOutputs(project)
   }
 }
 
