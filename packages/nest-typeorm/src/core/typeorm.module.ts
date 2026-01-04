5ef1cf0b7e1ca21f2c46b2a2de63cfaccef6330f
chufan <mmdapl@163.com>
2026-01-04 17:19:18 +0800

    feat(@142vip/nest-typeorm): 补充模块导出

--- packages/nest-typeorm/src/core/typeorm.module.ts
+++ packages/nest-typeorm/src/core/typeorm.module.ts
@@ -1,6 +1,9 @@
 import { DynamicModule, Module } from '@nestjs/common'
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
 
+export * from '@nestjs/typeorm'
+export * from 'typeorm'
+
 /**
  * 参考：
  * - https://docs.nestjs.cn/techniques/sql
--- packages/nest-typeorm/src/index.ts
+++ packages/nest-typeorm/src/index.ts
@@ -1,3 +1,2 @@
 export * from './core/data-type'
 export * from './core/typeorm.module'
-export * from '@nestjs/typeorm'
